78fae52ddf7da68c8dde5b98cbd4457ab282ad4d
ian6207 <ianv6207@mytusd.org>
2026-01-08 18:38:02 -0800

    Add badBoy-mode.js with mod title and description
    Initial creation of badBoy-mode.js with title and description elements.

---
+++ Modes/badBoy-mode.js
@@ -0,0 +1,11 @@
+//You know this kind of import and export, so do it on the top.
+const Modtitle = document.createElement("h1");
+Modtitle.textContent = "BAD BOY";
+Modtitle.style.fontSize = "50px";
+const ModetitleDes = document.createElement("h1");
+ModtitleDes.textContent = "An added mod by Ian";
+
+const ModSTYLE = document.createElement("style");
+ModSTYLE.innerHTML = "bod
+
+//In Progress
