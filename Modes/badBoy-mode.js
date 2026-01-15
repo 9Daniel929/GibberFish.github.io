b332c23cce96e2f89475cbe80296662abb69e77b
GibberFish <zakariyah6204@mytusd.org>
2026-01-15 15:53:32 -0800

    Update badBoy-mode.js

--- Modes/badBoy-mode.js
+++ Modes/badBoy-mode.js
@@ -1,1 +1,1 @@
-<h1>This is the badboy mod, if you are seeing this, you have uncovered a hidden mode that got deleted by the owner</h1>
+<small>This is the badboy mod, if you are seeing this, you have uncovered a hidden mode that got deleted by the owner<small>
